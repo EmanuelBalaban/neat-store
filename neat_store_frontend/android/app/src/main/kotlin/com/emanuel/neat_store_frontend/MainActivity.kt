64815ea4e402b1783d4fb04439741e35f16c7b75
Emanuel Balaban <emanuelbalaban@gmail.com>
2023-10-29 12:28:25 +0200

    Update gradle version

--- neat_store_frontend/android/app/src/main/kotlin/com/emanuel/neat_store_frontend/MainActivity.kt
+++ neat_store_frontend/android/app/src/main/kotlin/com/emanuel/neat_store_frontend/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.emanuel.neat_store_frontend
 
-import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity: FlutterFragmentActivity() {
