395118417beafb73c217f165e88dba8f6bf8eaa7
Emanuel Balaban <emanuelbalaban@gmail.com>
2023-10-29 12:24:58 +0200

    Add metadata to payment intent

--- neat-store-magento/src/app/code/NeatStore/GetPaymentIntent/Model/Resolver/GetPaymentIntent.php
+++ neat-store-magento/src/app/code/NeatStore/GetPaymentIntent/Model/Resolver/GetPaymentIntent.php
@@ -9,6 +9,7 @@ use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\Quote\Model\QuoteRepository;
 use Magento\Quote\Api\CartManagementInterface;
 use Magento\Quote\Model\QuoteIdMaskFactory;
+use Stripe\Customer;
 use Stripe\Exception\CardException;
 use Stripe\Stripe;
 use Stripe\PaymentIntent;
@@ -51,13 +52,38 @@ class GetPaymentIntent implements ResolverInterface
         // Initialize Stripe with your API key
         Stripe::setApiKey('sk_test_51O0nGfKUXnP0ANqYxf7L2oXKpUAwMpEroAPOhuvQuvtxwdEuMQzxaSTHubSqFSPUXHPrg2ji38WaTBT325RYfWLn00RDFr2XsF');
 
+        $quoteId = '#' . ($quote->getReservedOrderId() ?? $quote->reserveOrderId()->getReservedOrderId());
+        $customerFullName = "{$quote->getCustomerFirstname()} {$quote->getCustomerLastname()}";
+        $customerEmail = $quote->getCustomerEmail();
+        $shippingAddress = $quote->getShippingAddress();
+
         // Create a Payment Intent
         try {
             $paymentIntent = PaymentIntent::create([
                 'amount' => $quote->getGrandTotal() * 100, // Amount in cents
                 'currency' => $quote->getQuoteCurrencyCode(),
-                'description' => 'Payment for Cart',
-                // Additional options like 'metadata', 'receipt_email', etc.
+                'receipt_email' => $customerEmail,
+                'customer' => $this->getStripeCustomerId($customerEmail),
+                'description' => "Order $quoteId by $customerFullName",
+                'shipping' => [
+                    'address' => [
+                        'city' => $shippingAddress->getCity(),
+                        'country' => $shippingAddress->getCountryId(),
+                        'line1' => $shippingAddress->getStreetLine(0),
+                        'line2' => $shippingAddress->getStreetLine(1),
+                        'postal_code' => $shippingAddress->getPostcode(),
+                        'state' => $shippingAddress->getRegion()
+                    ],
+                    'name' => $customerFullName,
+                    'carrier' => $shippingAddress->getShippingMethod(),
+                    'phone' => $shippingAddress->getTelephone()
+                ],
+                'metadata' => [
+                    'order_id' => $quoteId,
+                    'magento_customer_id' => $quote->getCustomerId(),
+                    'customer_name' => $customerFullName,
+                    'customer_email' => $customerEmail
+                ],
             ]);
         } catch (CardException $e) {
             throw new Exception($e->getMessage());
@@ -65,10 +91,26 @@ class GetPaymentIntent implements ResolverInterface
 
         return [
             'id' => $paymentIntent->id,
+            'status' => $paymentIntent->status,
             'amount' => $paymentIntent->amount / 100, // Convert back to dollars
             'currency' => $paymentIntent->currency,
-            'status' => $paymentIntent->status,
             'clientSecret' => $paymentIntent->client_secret,
         ];
+    }
+
+    private function getStripeCustomerId(string $customerEmail): ?string
+    {
+        try {
+            // Use the Stripe Customer API to retrieve the customer by email
+            $customer = Customer::all(['email' => $customerEmail])->data[0];
+
+            if ($customer) {
+                return $customer->id;
+            } else {
+                throw new Exception("Stripe customer not found for email: $customerEmail");
+            }
+        } catch (Exception $e) {
+            return null;
+        }
     }
 }
